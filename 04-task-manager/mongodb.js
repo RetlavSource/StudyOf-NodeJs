555ebc626c08111efcb64e6e10bbc75c0a123457
RetlavSource <valterinfomundo@gmail.com>
2020-07-20 18:14:11 +0100

    finished lesson 79

--- 04-task-manager/mongodb.js
+++ 04-task-manager/mongodb.js
@@ -1,12 +1,11 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-// lesson's Server Discover and Monitoring engine option: { useNewUrlParser: true }
+// lesson's Server Discover and Monitoring engine option: { useNewUrlParser: true } - is the default option, so, no need to use
 // NEW Server Discover and Monitoring engine option: { useUnifiedTopology: true }
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -15,8 +14,27 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Valter',
-        age: 41
+    // db.collection('users').findOne({ _id: new ObjectID("5f15bd0e89af632fbaa83d0c") }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch!');
+    //     }
+
+    //     console.log(user);
+    // })
+
+    // db.collection('users').find({ age: 41 }).toArray((error, users) => {
+    //     console.log(users);
+    // })
+
+    // db.collection('users').find({ age: 41 }).count((error, count) => {
+    //     console.log(count);
+    // })
+
+    db.collection('tasks').findOne({ _id: new ObjectID("5f15b98ad1b19f2e90844d7a") }, (error, task) => {
+        console.log(task);
+    })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks);
     })
 })
