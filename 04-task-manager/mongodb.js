082138a7aa8d2363e4a5f31fb68c2f104bf4eb3d
RetlavSource <valterinfomundo@gmail.com>
2020-07-21 01:16:58 +0100

    finished lesson 81

--- 04-task-manager/mongodb.js
+++ 04-task-manager/mongodb.js
@@ -14,6 +14,40 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
     const db = client.db(databaseName);
 
+    // ****************************
+    // ********** CREATE **********
+
+    // db.collection('users').insertOne({ 
+    //     name: 'Andrew',
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user!');
+    //     }
+
+    //     console.log(result.ops);
+    // })
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Clean the house',
+    //         completed: true
+    //     },{
+    //         description: 'Renew inspection',
+    //         completed: false
+    //     }
+    // ], (error, result) => { 
+    //     if (error) {
+    //         return console.log('Unable to insert tasks!');
+    //     }
+
+    //     console.log(result.ops);
+    // })
+
+
+    // **************************
+    // ********** READ **********
+
     // db.collection('users').findOne({ _id: new ObjectID("5f15bd0e89af632fbaa83d0c") }, (error, user) => {
     //     if (error) {
     //         return console.log('Unable to fetch!');
@@ -30,11 +64,59 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
     //     console.log(count);
     // })
 
-    db.collection('tasks').findOne({ _id: new ObjectID("5f15b98ad1b19f2e90844d7a") }, (error, task) => {
-        console.log(task);
-    })
+    // db.collection('tasks').findOne({ _id: new ObjectID("5f15b98ad1b19f2e90844d7a") }, (error, task) => {
+    //     console.log(task);
+    // })
 
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        console.log(tasks);
+    // db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+    //     console.log(tasks);
+    // })
+
+
+    // ****************************
+    // ********** UPDATE **********
+
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID("5f15b1fd5832f02d7b4e73df")
+    // }, {
+    //     $inc: {
+    //         age: -19
+    //     }
+    // }).then((result) => {
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log(error);
+    // })
+
+    // db.collection('tasks').updateMany({
+    //     completed: false
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }).then((result) => {
+    //     console.log(result.modifiedCount);
+    // }).catch((error) => {
+    //     console.log(error);
+    // })
+
+
+    // ****************************
+    // ********** DELETE **********
+
+    // db.collection('users').deleteMany({
+    //     age: 23
+    // }).then((result) => {
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log(error);
+    // })
+
+    db.collection('tasks').deleteOne({
+        description : "Clean the house"
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
     })
 })
