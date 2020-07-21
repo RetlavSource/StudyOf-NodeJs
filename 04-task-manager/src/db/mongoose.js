24a4d054d38ed5bad86383adb7c5342a595a1d63
RetlavSource <valterinfomundo@gmail.com>
2020-07-21 18:54:03 +0100

    finished lesson 90

--- 04-task-manager/src/db/mongoose.js
+++ 04-task-manager/src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+// const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,57 +7,81 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 });
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid!');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number!');
-            }
-        }
-    }
-})
+// ********************************
+// ********** USER model **********
 
-const me = new User({
-    name: '   Valter    ',
-    email: 'MYEMAIL@mrbitwise.IO    '
-})
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('Email is invalid!');
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         minlength: 7,
+//         trim: true,
+//         validate(value) {
+//             if (value.toLowerCase().includes('password')) {
+//                 throw new Error('Password cannot contain "password"!');
+//             }
+//         }
 
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log('Error!', error);
-})
+//     },
+//     age: {
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if (value < 0) {
+//                 throw new Error('Age must be a positive number!');
+//             }
+//         }
+//     }
+// })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-})
+// const me = new User({
+//     name: '   Valter    ',
+//     email: 'MYEMAIL@mrbitwise.IO    ',
+//     password: 'phone089!'
+// })
+
+// me.save().then(() => {
+//     console.log(me);
+// }).catch((error) => {
+//     console.log('Error!', error);
+// })
+
+
+
+
+// ********************************
+// ********** TASK model **********
+
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     }
+// })
 
 // const task = new Task({
-//     description: 'Learn the Mongoose library',
-//     completed: false
+//     description: '    Eat lunch.'
 // })
 
 // task.save().then(() => {
