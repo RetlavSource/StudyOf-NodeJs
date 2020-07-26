3d636707936e1b8588602bf824e8c7e5cf910a50
RetlavSource <valterinfomundo@gmail.com>
2020-07-26 03:27:05 +0100

    finished lesson 104

--- 04-task-manager/src/index.js
+++ 04-task-manager/src/index.js
@@ -1,91 +1,30 @@
 const express = require('express');
 require('./db/mongoose'); // Only connects to the mongodb database
-const User = require('./models/user')
-const Task = require('./models/task');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-// Create a User
-app.post('/users', async (req, res) => {
-    const user = new User(req.body);
-
-    try {
-        await user.save();
-        res.status(201).send(user);
-    } catch (e) {
-        res.status(400).send(e);
-    }
-});
-
-// Get ALL Users
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-// Get a User by ID
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const user = await User.findById(_id);
-
-        if (!user) {
-            return res.status(404).send();
-        }
-
-        res.send(user);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-// Create a Task
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body);
-
-    try {
-        await task.save();
-        res.status(201).send(task);
-    } catch (e) {
-        res.status(400).send(e);
-    }
-});
-
-// Get ALL Tasks
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({});
-        res.send(tasks);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-// Get a Task by ID
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id;
-
-    try {
-        const task = await Task.findById(_id);
-
-        if (!task) {
-            return res.status(404).send();
-        }
-
-        res.send(task);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
 });
+
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+    const password = 'Red12345!';
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(password);
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare('Red12345!', hashedPassword);
+    console.log(isMatch);
+}
+
+myFunction();
