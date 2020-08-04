0d4d4705229bd2d7fc5982db1b68e82fd758b6a5
RetlavSource <valterinfomundo@gmail.com>
2020-08-04 06:27:38 +0100

    finished lesson 123

--- 04-task-manager/src/index.js
+++ 04-task-manager/src/index.js
@@ -6,17 +6,16 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disable!');
-//     } else {
-//         next();
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is under maintenance. Please try again later!');
-// });
+const multer = require('multer');
+const upload = multer({
+    dest: 'images'
+});
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send();
+});
+
 
 app.use(express.json());
 app.use(userRouter);
@@ -25,18 +24,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
 });
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-// const main = async () => {
-//     // const task = await Task.findById('5f24f651f2ebc323ae62761a');
-//     // await task.populate('owner').execPopulate();
-//     // console.log(task.owner);
-
-//     const user = await User.findById('5f24f583c70779235d385850');
-//     await user.populate('tasks').execPopulate();
-//     console.log(user.tasks);
-// }
-
-// main();
